99b62c37611bde0ce451bb49a44dac0558d520cb
phuoc_develop <hacongphuoc2409@gmail.com>
2022-05-10 01:47:34 +0700

    coding fe 10/05

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,55 @@
 // @ts-nocheck
 import React from 'react'
+import Helmet from '../components/Helmet';
+import Grid from 'components/Grid';
+import ProductCard from 'components/ProductCard';
+import Section, {SectionTitle,SectionBody} from 'components/Section';
+import productData from 'assets/fake-data/products';
+import ProductView from 'components/ProductView';
+const Product = props => {
 
-const Product = () => {
+  const product = productData.getProductBySlug(props.match.params.slug)
+
+  const relatedProducts = productData.getProducts(8)
+
+  React.useEffect(() => {
+    window.scrollTo(0,0)
+  }, [product])
+
   return (
-   <div>Product</div>
+   <Helmet title={product.title}>
+     <Section>
+       <SectionBody>
+        <ProductView product={product}/>
+       </SectionBody>
+     </Section>
+     <Section>
+       <SectionTitle>
+         Khám phá thêm
+       </SectionTitle>
+       <SectionBody>
+         <Grid
+          col={4}
+          mdCol={2}
+          smCol={1}
+          gap={20}
+         >
+           {
+             relatedProducts.map((item,index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+             ))
+           }
+         </Grid>
+       </SectionBody>
+     </Section>
+   </Helmet>
   )
 }
 
